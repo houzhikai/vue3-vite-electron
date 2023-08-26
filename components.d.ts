7b63e7d1c1801cdd027e75e819f472badfe31b37
侯志凯 <hou17513213314@163.com>
2023-08-27 00:50:59 +0800

    fix: 修改启动项目的命令

--- components.d.ts
+++ components.d.ts
@@ -5,19 +5,19 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 export {}
 
-declare module 'vue' {
+declare module "vue" {
   export interface GlobalComponents {
-    copy: typeof import('./src/components/WelcomeItem copy.vue')['default']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
-    IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
-    IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
-    IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
-    IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
-    IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
-    RouterLink: typeof import('vue-router')['RouterLink']
-    RouterView: typeof import('vue-router')['RouterView']
-    TheWelcome: typeof import('./src/components/TheWelcome.vue')['default']
-    WelcomeItem: typeof import('./src/components/WelcomeItem.vue')['default']
-    WelcomeItem2: typeof import('./src/components/WelcomeItem2.vue')['default']
+    copy: typeof import("./src/components/WelcomeItem copy.vue")["default"];
+    HelloWorld: typeof import("./src/components/HelloWorld.vue")["default"];
+    IconCommunity: typeof import("./src/components/icons/IconCommunity.vue")["default"];
+    IconDocumentation: typeof import("./src/components/icons/IconDocumentation.vue")["default"];
+    IconEcosystem: typeof import("./src/components/icons/IconEcosystem.vue")["default"];
+    IconSupport: typeof import("./src/components/icons/IconSupport.vue")["default"];
+    IconTooling: typeof import("./src/components/icons/IconTooling.vue")["default"];
+    RouterLink: typeof import("vue-router")["RouterLink"];
+    RouterView: typeof import("vue-router")["RouterView"];
+    TheWelcome: typeof import("./src/components/TheWelcome.vue")["default"];
+    WelcomeItem: typeof import("./src/components/WelcomeItem.vue")["default"];
+    WelcomeItem2: typeof import("./src/components/WelcomeItem2.vue")["default"];
   }
 }
